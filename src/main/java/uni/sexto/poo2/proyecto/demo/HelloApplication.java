e401681761d07534cec51b31d212ed77cfea98c7
Camachess <camachocastelanjm@gmail.com>
2025-05-02 22:04:52 -0600

    First Commit

---
+++ src/main/java/uni/sexto/poo2/proyecto/demo/HelloApplication.java
@@ -0,0 +1,9 @@
+package uni.sexto.poo2.proyecto.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
