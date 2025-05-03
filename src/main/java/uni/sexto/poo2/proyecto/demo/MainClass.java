cdb7bd81001201f5828d8242c31b7629cbdf87b1
Camachess <camachocastelanjm@gmail.com>
2025-05-03 00:36:03 -0600

    Read and create Deque for all of the URLs in the given file

--- src/main/java/uni/sexto/poo2/proyecto/demo/MainClass.java
+++ src/main/java/uni/sexto/poo2/proyecto/demo/MainClass.java
@@ -5,10 +5,21 @@ import uni.sexto.poo2.proyecto.demo.ReadResources;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+
 public class MainClass {
-    public static void main (String [] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         Deque<String> a = ReadResources.ReadFileToDeque("demo/src/main/java/uni/sexto/poo2/proyecto/demo/", "urls.txt");
         System.out.println(a);
+        for (String line : a) {
+            String url = line;
+            System.out.println(url);
+        }
     }
+
 
 }
